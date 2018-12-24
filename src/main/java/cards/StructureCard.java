c6891d98ab480aa27197a19ffbb4112aebf202df
Michael Wang <michael1.wang@scotiabank.com>
2018-12-24 11:09:16 -0500

    added structure card

---
+++ src/main/java/cards/StructureCard.java
@@ -0,0 +1,13 @@
+package cards;
+
+import java.util.List;
+
+import resources.Age;
+import resources.Resource;
+
+public class StructureCard extends Card {
+  public StructureCard(String name, Age age, List<Resource> cost, List<Resource> production) {
+    super(name, age, cost, production);
+    // TODO Auto-generated constructor stub
+  }
+}
